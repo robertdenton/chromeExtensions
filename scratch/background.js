c889cb410de6f8ab0400898b21cc2e43b531a2ce
Robert Denton <robert.denton9@gmail.com>
2014-02-19 00:13:51 -0700

    gets rid of many visual elements and changes some of the text to comic sans. Still need to get stories to change to comic sans though

--- scratch/background.js
+++ scratch/background.js
@@ -1,2 +1,46 @@
 console.log('hello');
-$("h1").remove();
+$(".listingArticleBlurb").remove();
+$(".listing5ImageBox").remove();
+$("#header").remove();
+$("#outerRightRegion").remove();
+$("#footer").remove();
+$("#jBar").remove();
+$("#jBarRetract").remove();
+$("#postHeader1").remove();
+$("#preHeader").remove();
+$(".was-figure").remove();
+$(".excerpt").remove();
+$(".carousel").remove();	
+$("#sponsorLinks").remove();
+$("#riseUpAd").remove();
+$(".ad").remove();
+$("#recommended").remove();
+$(".articlePosition1").remove();
+$("img").remove();
+$("iframe").remove();
+$(".articlePositionHeader").remove();
+$(".articlePosition1").remove();
+$(".articlePosition2").remove();
+$(".articlePosition3").remove();
+$(".articlePosition4").remove();
+$(".articlePosition5").remove();
+$(".articlePosition6").remove();
+$(".articlePosition7").remove();
+$(".articlePosition8").remove();
+$(".articlePosition9").remove();
+$(".articlePosition10").remove();
+$("#mc-embed-container").remove();
+
+$("*").each(function(){
+	$(this).css("font-family","Comic Sans MS");
+});
+$(".articleBody").css("font-family","Comic Sans MS");
+$("body").css("font-family","Comic Sans MS");
+
+window.addEventListener('load', function (e) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'newstyle.css';
+    document.head.appendChild(link);
+}, false);
